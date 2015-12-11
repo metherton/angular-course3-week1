fbc608e3f7cdfa76c2019c63cece1fe697e56000
metherton <martinetherton63@gmail.com>
2015-12-12 00:50:22 +0100

    added form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -108,7 +108,7 @@ angular.module('confusionApp', [])
 
             var dish={
                           name:'Uthapizza',
-                          image: 'images/uthapizza.png',
+                              image: 'images/uthapizza.png',
                           category: 'mains', 
                           label:'Hot',
                           price:'4.99',
@@ -155,14 +155,20 @@ angular.module('confusionApp', [])
         .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
+
+            $scope.comment = {
+                author: "",
+                rating: 5,
+                comment: ""
+            };
             
             $scope.submitComment = function () {
                 
                 //Step 2: This is how you record the date
-                "The date property of your JavaScript object holding the comment" = new Date().toISOString();
-                
+                $scope.comment.date = new Date().toISOString();
+
                 // Step 3: Push your comment into the dish's comment array
-                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push($scope.comment);
                 
                 //Step 4: reset your form to pristine
                 
